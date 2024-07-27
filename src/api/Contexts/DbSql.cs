4dd1dd238b4b2c125a14d5621b5d84760ac64087
Manibharathi <manibharathidct@gmail.com>
2024-07-27 22:14:40 +0530

    Add the dbcontext

--- src/api/Contexts/DbSql.cs
+++ src/api/Contexts/DbSql.cs
@@ -12,6 +12,12 @@ namespace api.Contexts
         #region DbSets
         public DbSet<User> Users { get; set; }
         public DbSet<UserAuth> UserAuths { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyMediaFile> MediaFiles { get; set; }
+        public DbSet<PropertyLand> Lands { get; set; }
+        public DbSet<PropertyHome> Homes { get; set; }
+        public DbSet<PropertyAmenity> Amenities { get; set; }
+
         #endregion
 
         #region OnModelCreating
@@ -26,6 +32,10 @@ namespace api.Contexts
                 .HasOne(x => x.UserAuth)
                 .WithOne(x => x.User)
                 .HasForeignKey<UserAuth>(x => x.UserId);
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.Property)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId);
             #endregion
 
             #region UserAuth
@@ -34,6 +44,64 @@ namespace api.Contexts
             modelBuilder.Entity<UserAuth>()
                 .HasKey(x => x.UserId);
             #endregion
+
+            #region property
+            modelBuilder.Entity<Property>().ToTable("Properties");
+
+            modelBuilder.Entity<Property>()
+                .HasKey(x => x.PropertyId);
+            modelBuilder.Entity<Property>()
+                .HasMany(x => x.Amenities)
+                .WithOne(x => x.Property)
+                .HasForeignKey(x => x.PropertyId);
+            modelBuilder.Entity<Property>()
+                .HasMany(x => x.MediaFiles)
+                .WithOne(x => x.Property)
+                .HasForeignKey(x => x.PropertyId);
+            modelBuilder.Entity<Property>()
+                .HasOne(x => x.Home)
+                .WithOne(x => x.Property)
+                .HasForeignKey<PropertyHome>(x => x.PropertyId);
+            modelBuilder.Entity<Property>()
+                .HasOne(x => x.Land)
+                .WithOne(x => x.Property)
+                .HasForeignKey<PropertyLand>(x => x.PropertyId);
+
+            #region Property Attributes
+            modelBuilder.Entity<Property>()
+                .Property(x => x.Price)
+                .HasColumnType("decimal(18,2)");
+            #endregion
+
+            #endregion
+
+            #region MediaFile
+            modelBuilder.Entity<PropertyMediaFile>().ToTable("MediaFiles");
+            modelBuilder.Entity<PropertyMediaFile>()
+                .HasKey(x => x.MediaFileId);
+            #endregion
+
+            #region Land
+            modelBuilder.Entity<PropertyLand>().ToTable("Lands");
+            modelBuilder.Entity<PropertyLand>()
+                .HasKey(x => x.PropertyId);
+
+            #endregion
+
+            #region Home
+            modelBuilder.Entity<PropertyHome>().ToTable("Homes");
+            modelBuilder.Entity<PropertyHome>()
+                .HasKey(x => x.PropertyId);
+            #endregion
+
+            #region Amenity
+            modelBuilder.Entity<PropertyAmenity>().ToTable("Amenities");
+            modelBuilder.Entity<PropertyAmenity>()
+                .HasKey(x => x.AmenityId);
+            #endregion
+
+
+
         }
         #endregion
     }
--- src/api/Migrations/20240727124928_add_property.Designer.cs
+++ src/api/Migrations/20240727124928_add_property.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api.Contexts;
 
@@ -11,9 +12,10 @@ using api.Contexts;
 namespace api.Migrations
 {
     [DbContext(typeof(DbSql))]
-    partial class DbSqlModelSnapshot : ModelSnapshot
+    [Migration("20240727124928_add_property")]
+    partial class add_property
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
