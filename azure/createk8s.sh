8c68e872e88f8b28f8df8aa90d2c316d81f2194d
Manibharathi <manibharathidct@gmail.com>
2024-07-31 09:33:50 +0530

    config the k8s deployement

--- azure/createk8s.sh
+++ azure/createk8s.sh
@@ -1,6 +1,11 @@
-clusterName="ks-aksmani"
+clusterName="k8s-aksmani"
 resourceGroup="rg-aksmani"
 location="eastus"
 
-az aks create --name $clusterName --resource-group $resourceGroup --location $location --network-plugin azure --network-plugin-mode overlay --pod-cidr 192.168.0.0/16 --generate-ssh-keys
 
+# Create a resource group
+az group create --name $resourceGroup --location $location
+
+# Create a new AKS cluster with network policy enabled by default  plugin overlay and pod-cidr 
+
+az aks create --name $clusterName --resource-group $resourceGroup --location $location --network-plugin azure --network-plugin-mode overlay --pod-cidr 192.168.0.0/16 --generate-ssh-keys
